bff824ff53a7f433aa965224fdeaedee0094e46a
nonex97 <ngrguric97@gmail.com>
2026-06-19 19:33:46 +0200

    Created initial class libraries and an enum to hold spot status.

---
+++ BattleShipLiteLibrary/Models/PlayerInfoModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BattleShipLiteLibrary.Models
+{
+    public class PlayerInfoModel
+    {
+        public string PlayersName { get; set; }
+        public List<GridSpotModel> ShipLocations { get; set; }
+        public List<GridSpotModel> ShotGrid { get; set; }
+    }
+}
